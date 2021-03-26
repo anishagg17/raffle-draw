d47b77304f6e5f3ebe83e08a7023e3f4a5899e94
Anish Aggarwal <anish17122000@gmail.com>
2021-03-26 16:01:17 +0530

    updated event controller

--- api/event/index.js
+++ api/event/index.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     const events = await Event.find({status:'active'}).sort({ start_date: 1 }).limit(10);
     return res.json(events);
   } catch (err) {
-    res.status(401).json({
+    return res.status(401).json({
       msg: 'Server Error'
     });
   }
@@ -23,9 +23,48 @@ router.post('/', async (req, res) => {
     const result = await Event.create(event);
 
     // returning the new Event
-    res.json({ result });
+    return res.json({ result });
   } catch (err) {
-    res.status(400).end(err.errmsg);
+    return res.status(400).end(err.errmsg);
+  }
+});
+
+//Pick a Winner
+// router.post('/', async (req, res) => {
+//   try {
+//     const {name, start_date} = req.body;
+//     const event = { name, start_date };
+//     // Creating the Event object
+//     const result = await Event.create(event);
+
+//     // returning the new Event
+//     return res.json({ result });
+//   } catch (err) {
+//     return res.status(400).end(err.errmsg);
+//   }
+// });
+
+//List all the winners of all the events in the last one week.
+router.get('/winners', async (req, res) => {
+  try {
+    const startDate = new Date();
+    console.log(startDate);
+    startDate.setDate(startDate.getDate()-7);
+
+    let events = await Event.find({
+      status:'closed', 
+      start_date:{
+        $gte: startDate
+      }
+    }).sort({ start_date: 1 })
+    .populate('winner');
+
+    events = events.filter(({winner}) => (typeof winner != 'undefined'));
+    return res.json(events);
+  } catch (err) {
+    return res.status(401).json({
+      msg: 'Server Error'
+    });
   }
 });
 
